bb7a5833e349dc63b95b2a4fcff61352be88c2d5
naoya-Ito-cxx <naoyait3@gmail.com>
2025-02-01 15:54:01 +0900

    NotePage をすこし見やすくしました

--- Views/Home.xaml.cs
+++ Views/Home.xaml.cs
@@ -2,18 +2,36 @@ namespace News_Radio_app.Views;
 
 public partial class Home : Window
 {
-	public Home()
-	{
-		InitializeComponent();
-		Page = new ContentPage()
-		{
-			Content = new VerticalStackLayout
-			{
-				Children = {
-					new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
-					}
-				}
-			}
-		};
-	}
+    public Home()
+    {
+        InitializeComponent();
+        Page = new ContentPage()
+        {
+            Content = new VerticalStackLayout
+            {
+                Children = {
+                    new Label
+                    {
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.Start,
+                        Text = "Welcome News Radio App!!",
+                        FontSize = 30  // 文字サイズを30に設定
+                    },
+                    new BoxView
+                    {
+                        Color = Colors.Black,  // 線の色を黒に設定
+                        HeightRequest = 2,     // 線の太さ（高さ）
+                        HorizontalOptions = LayoutOptions.Fill // 横幅を画面いっぱいに広げる
+                    },
+                    new Label
+                    {
+                        HorizontalOptions = LayoutOptions.End, // 右詰めに配置
+                        VerticalOptions = LayoutOptions.Start, // 上部に配置
+                        Text = "Yamaguti, Naoya",
+                        FontSize = 12  // 文字サイズを12に設定
+                    }
+                }
+            }
+        };
+    }
 }
