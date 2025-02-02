0308abc6c58599a78054204ddee94f31897451b6
naoya-Ito-cxx <naoyait3@gmail.com>
2025-02-02 20:53:13 +0900

    Homeページが動くようになりました

--- Views/Home.xaml.cs
+++ Views/Home.xaml.cs
@@ -1,37 +1,10 @@
-namespace News_Radio_app.Views;
-
-public partial class Home : Window
+namespace News_Radio_app.Views
 {
-    public Home()
+    public partial class Home : ContentPage
     {
-        InitializeComponent();
-        Page = new ContentPage()
+        public Home()
         {
-            Content = new VerticalStackLayout
-            {
-                Children = {
-                    new Label
-                    {
-                        HorizontalOptions = LayoutOptions.Center,
-                        VerticalOptions = LayoutOptions.Start,
-                        Text = "Welcome News Radio App!!",
-                        FontSize = 30  // 文字サイズを30に設定
-                    },
-                    new BoxView
-                    {
-                        Color = Colors.Black,  // 線の色を黒に設定
-                        HeightRequest = 2,     // 線の太さ（高さ）
-                        HorizontalOptions = LayoutOptions.Fill // 横幅を画面いっぱいに広げる
-                    },
-                    new Label
-                    {
-                        HorizontalOptions = LayoutOptions.End, // 右詰めに配置
-                        VerticalOptions = LayoutOptions.Start, // 上部に配置
-                        Text = "Yamaguti, Naoya",
-                        FontSize = 12  // 文字サイズを12に設定
-                    }
-                }
-            }
-        };
+            InitializeComponent();
+        }
     }
 }
